b742953732e0a3d7ed5f8f13074509a9a011330e
EC2 Default User <ec2-user@ip-172-31-23-250.eu-west-2.compute.internal>
2021-04-19 10:23:00 +0000

    Printing issue sorted

--- directory.rb
+++ directory.rb
@@ -1,18 +1,3 @@
-#Lets put all the students in an array
-#students = [
-# {name: "Dr. Hannibal Lecter", cohort: :november},
-# {name: "Darth Vader", cohort: :november},
-# {name: "Nurse Ratched", cohort: :november},
-# {name: "Michael Corleone", cohort: :november},
-# {name: "Alex DeLarge", cohort: :november},
-# {name: "The Wicked Witch of the West", cohort: :november},
- #{name: "Terminator", cohort: :november},
- #{name: "Freddy Kruger", cohort: :november},
- #{name: "The Joker", cohort: :november},
-# {name: "Joffrey Baratheon", cohort: :november},
- #{name: "Norman Bates", cohort: :november}
- #]
-
 
  def input_students
   puts "Please enter the names of the students"
@@ -20,23 +5,29 @@
   students = []
   name = gets.chomp
   while !name.empty? do
-   students << {name: name, cohort: :november}
-   puts "Now we have #{students.count} students"
-   name = gets.chomp
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    name = gets.chomp
   end
-   students
+ students
  end
 
- def print_header
-  puts "The students of Villains Acadamy"
-  puts "-----------"
- end
+
  def print(students)
  students.each do |student|
    puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
  end
- def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+
+ def print_header
+  puts "The students of my cohort at Makers"
+  puts "-----------"
  end
- students = input_students
+
+ def print_footer(students)
+  puts "Overall, we have #{students.count} great students"
+ end
+students = input_students
+print_header
+print students
+print_footer(students)
