3fb39b4a774271d56749c411eca4aa65789c1c5e
EC2 Default User <ec2-user@ip-172-31-23-250.eu-west-2.compute.internal>
2021-04-20 09:51:23 +0000

    Save method added

--- directory.rb
+++ directory.rb
@@ -1,11 +1,22 @@
 @students = []
 
+def save_students
+  file = File.open("students.csv", "w")
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+   end
+   file.close
+end
 def process(selection)
   case selection
     when "1"
       input_students
     when "2"
       show_students
+    when "3"
+     save_students
     when "9"
       exit
     else
@@ -15,6 +26,7 @@ end
 def print_menu
   puts "1. Input students"
   puts "2. Show the students"
+  puts "3. Save the list of students.csv"
   puts "9. Exit"
 end
 
