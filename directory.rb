7529df9000f8dc8dc2991a15ef01026a19b56175
EC2 Default User <ec2-user@ip-172-31-23-250.eu-west-2.compute.internal>
2021-04-20 09:34:34 +0000

    First refactoring done

--- directory.rb
+++ directory.rb
@@ -1,48 +1,79 @@
+@students = []
+
+def process(selection)
+  case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "9"
+      exit
+    else
+      puts "I don't know what you meant, try again"
+  end
+end
+def print_menu
+  puts "1. Input students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_student_list
+  print_footer
+end
+
+def interactive_menu
+   loop do
+     print_menu
+     process(gets.chomp)
+  end
+end
 
  def input_students
   puts "Please enter the names of the student"
   puts "To finish, just hit return twice"
-  students = []
-  name = String.new(gets).strip
+  name = gets.chomp
   puts "Which cohort will this student join?"
-  cohort = String.new(gets).strip
+  cohort = gets.chomp
   if cohort.empty?
    cohort = "unknown"
   end
   while !name.empty? do
-   students << {name: name, cohort: cohort}
-   if students.count > 1
-    puts "Now we have #{students.count} students"
+   @students << {name: name, cohort: cohort}
+   if @students.count > 1
+    puts "Now we have #{@students.count} students"
    else
-   puts "Now we have #{students.count} student"
+   puts "Now we have #{@students.count} student"
    end
-  name = String.new(gets).strip
+  name = gets.chomp
   puts "Which cohort will this student join?"
-  cohort = String.new(gets).strip
+  cohort = gets.chomp
 
    if cohort.empty?
     cohort = "unknown"
    end
   end
-  students
+  @students
  end
 
-
- def print(students)
-  students.each_with_index do |student, index|
-   puts "#{index}.#{student[:name]} #{student[:cohort]} cohort".center(60)
+def print_student_list()
+  if @students.empty?
+    puts "No students have been registered".center(60)
+  else
+    @students.each do |student|
+    puts "#{student[:name]} #{student[:cohort]} cohort".center(60)
+    end
   end
- end
+end
 
  def print_header
   puts "The students of my cohort at Makers".center(60, "-")
   puts "-----------".center(60)
  end
 
- def print_footer(students)
-  puts "Overall, we have #{students.count} great students".center(60)
+ def print_footer
+  puts "Overall, we have #{@students.count} great students".center(60)
  end
-students = input_students
-print_header
-print(students)
-print_footer(students)
+interactive_menu
