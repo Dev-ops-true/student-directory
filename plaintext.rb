29287c46dd49e8dc32c9ab6e1b9f15cfaf141cb9
EC2 Default User <ec2-user@ip-172-31-23-250.eu-west-2.compute.internal>
2021-04-20 08:56:35 +0000

    If statement for print function installed

--- plaintext.rb
+++ plaintext.rb
@@ -23,8 +23,12 @@ def print_header
 end
 
 def print(students)
-  students.each do |student|
+  if students.empty?
+    puts "No students have been registered"
+  else
+    students.each do |student|
     puts "#{student[:name]} #{student[:cohort]} cohort"
+    end
   end
 end
 
